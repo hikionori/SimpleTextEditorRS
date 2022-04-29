473c94c04d2ca37a40cbad76e112fc6021f467f7
Danila (hikionori) Verbinskiy <danv4656@gmail.com>
2022-04-29 04:40:49 +0300

    Simple text editor

---
+++ target/debug/deps/sys-57c76139707bc862.d
@@ -0,0 +1,7 @@
+C:\Users\Danila\Documents\WorkDir\Rust\text_editor\target\debug\deps\sys-57c76139707bc862.rmeta: C:\Users\Danila\.cargo\registry\src\github.com-1ecc6299db9ec823\sys-0.0.0\src\lib.rs
+
+C:\Users\Danila\Documents\WorkDir\Rust\text_editor\target\debug\deps\libsys-57c76139707bc862.rlib: C:\Users\Danila\.cargo\registry\src\github.com-1ecc6299db9ec823\sys-0.0.0\src\lib.rs
+
+C:\Users\Danila\Documents\WorkDir\Rust\text_editor\target\debug\deps\sys-57c76139707bc862.d: C:\Users\Danila\.cargo\registry\src\github.com-1ecc6299db9ec823\sys-0.0.0\src\lib.rs
+
+C:\Users\Danila\.cargo\registry\src\github.com-1ecc6299db9ec823\sys-0.0.0\src\lib.rs:
---
+++ target/debug/deps/text_editor.d
@@ -0,0 +1,13 @@
+C:\Users\Danila\Documents\WorkDir\Rust\text_editor\target\debug\deps\text_editor.exe: src\main.rs src\document.rs src\editor.rs src\filetype.rs src\highlighting.rs src\row.rs src\terminal.rs
+
+C:\Users\Danila\Documents\WorkDir\Rust\text_editor\target\debug\deps\text_editor.d: src\main.rs src\document.rs src\editor.rs src\filetype.rs src\highlighting.rs src\row.rs src\terminal.rs
+
+src\main.rs:
+src\document.rs:
+src\editor.rs:
+src\filetype.rs:
+src\highlighting.rs:
+src\row.rs:
+src\terminal.rs:
+
+# env-dep:CARGO_PKG_VERSION=0.1.0
